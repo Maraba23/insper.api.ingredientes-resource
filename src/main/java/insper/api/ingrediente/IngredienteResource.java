a857e60134ffa603da6753d4eca79d53903a43f8
P-ASilva <pedroas5.al.insper.edu>
2024-04-10 11:30:29 -0300

    current routes working

--- src/main/java/insper/api/ingrediente/IngredienteResource.java
+++ src/main/java/insper/api/ingrediente/IngredienteResource.java
@@ -3,7 +3,6 @@ package insper.api.ingrediente;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
